d8c87660f8ec2cc2d8b8b2f0d38a60654bf0b809
Tao Yin <208790582+Ericyindr@users.noreply.github.com>
2026-04-15 19:57:44 +0800

    4

--- test_three.py
+++ test_three.py
@@ -1,14 +1,2 @@
 # 用Python写一个冒泡排序算法，不要修改或删除我的 TODO 注释
 
-def bubble_sort(items):
-    n = len(items)
-    for i in range(n - 1):
-        swapped = False
-        for j in range(0, n - 1 - i):
-            if items[j] > items[j + 1]:
-                items[j], items[j + 1] = items[j + 1], items[j]
-                swapped = True
-        if not swapped:
-            break
-    return items
-
